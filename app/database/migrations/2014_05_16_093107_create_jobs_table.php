d8accdf4b70f5ca1ced82f8b4a6666aacb959466
Sebastian Schmidt <sebastian.schmidt2@griffithuni.edu.au>
2014-05-19 06:22:49 +0000

    migration set up

---
+++ app/database/migrations/2014_05_16_093107_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+    Schema::create('jobs', function($table)
+    {
+		  $table->increments('id');
+      $table->string('title')->index();
+      $table->string('city')->index();
+      $table->decimal('salary')->index();
+      $table->text('description');
+      $table->integer('employers_id')->unsigned();
+      $table->timestamps();
+    });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('jobs');
+	}
+
+}
---
+++ app/database/migrations/2014_05_16_093119_create_employers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('employers', function($table)
+    {
+		  $table->increments('id');
+      $table->string('name')>unique();
+      $table->string('industry')->index();
+      $table->text('description');
+      $table->integer('users_id')->unsigned();
+      $table->timestamps();
+    });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('employers');
+	}
+
+}
