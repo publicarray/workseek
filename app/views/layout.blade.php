0e4f53e30177116c282c0956e50aab75dbd4b7b1
Sebastian Schmidt <sebastian.schmidt2@griffithuni.edu.au>
2014-07-14 16:41:26 +1000

    mobile navigation height and include author

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <link rel="shortcut icon" href="favicon.ico" />
+    <meta name="author" content="Sebastian Schmidt">
     <link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/cosmo/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:300">
     {{ HTML::style('assets/css/style.css') }}
