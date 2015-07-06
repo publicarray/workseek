5cdcbbcbd75fdac40f95e9387f622377aff0e4e5
Sebastian Schmidt <publicarray@icloud.com>
2015-07-06 12:01:27 +1000

    use bower, npm and gulp in development

--- laravel/gulpfile.js
+++ laravel/gulpfile.js
@@ -1,5 +1,7 @@
 var elixir = require('laravel-elixir');
 
+var bowerDir = './resources/assets/bower/';
+var publicDir = './public/assets/';
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -12,5 +14,14 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.copy(bowerDir + 'jquery/dist/jquery.min.js', publicDir + 'js/jquery.min.js')
+
+        .copy(bowerDir + 'bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js', publicDir + 'js/bootstrap-datepicker.min.js')
+        .copy(bowerDir + 'bootstrap-datepicker/dist/css/bootstrap-datepicker.standalone.css', publicDir + 'css/bootstrap-datepicker.min.css')
+
+        .copy(bowerDir + 'bootstrap/dist/js/bootstrap.min.js', publicDir + 'js/bootstrap.min.js')
+        .copy(bowerDir + 'bootswatch/cosmo/bootstrap.min.css', publicDir + 'css/bootstrap.min.css') // cosmo boostrap theme
+        .copy(bowerDir + 'bootswatch/fonts', publicDir + 'fonts')
+
+        .copy(bowerDir + 'timeago/jquery.timeago.js', publicDir + 'js/jquery.timeago.js'); // requires jquery
 });
